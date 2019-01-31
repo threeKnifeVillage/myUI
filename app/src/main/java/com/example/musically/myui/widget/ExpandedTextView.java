c0d16c313b08f4280a3f1f85626e19d8fb7ecc17
王磊 <Peter.Wang@musical.ly>
2019-01-31 15:11:32 +0800

    feat:增加动画

--- app/src/main/java/com/example/musically/myui/widget/ExpandedTextView.java
+++ app/src/main/java/com/example/musically/myui/widget/ExpandedTextView.java
@@ -7,10 +7,12 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
+import android.view.animation.Transformation;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -159,6 +161,7 @@ public class ExpandedTextView extends LinearLayout implements View.OnClickListen
 
             @Override
             public void onAnimationEnd(Animation animation) {
+                clearAnimation();
                 mIsAnimation = false;
             }
 
@@ -192,7 +195,16 @@ public class ExpandedTextView extends LinearLayout implements View.OnClickListen
             mTargetView = targetView;
             mStartHeight = startHeight;
             mEndHeight = endHeight;
+            setDuration(3000);
         }
 
+        @Override
+        protected void applyTransformation(float interpolatedTime, Transformation t) {
+            Log.e("wang.lei", "interpolatedTime is " + interpolatedTime);
+            final int newHeight = (int) ((mEndHeight - mStartHeight) * interpolatedTime + mStartHeight);
+            mTv.setMaxLines(newHeight - mMarginBetweenTextAndBottom);
+            mTv.getLayoutParams().height = newHeight;
+            mTv.requestLayout();
+        }
     }
 }
